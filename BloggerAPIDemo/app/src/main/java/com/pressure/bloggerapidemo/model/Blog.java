5c66270ea19f290b8056703ac1a318d3702fccbb
Harshasripraneeth <46346478+Harshasripraneeth@users.noreply.github.com>
2020-08-01 16:43:25 +0530

    model

--- BloggerAPIDemo/app/src/main/java/com/pressure/bloggerapidemo/model/Blog.java
+++ BloggerAPIDemo/app/src/main/java/com/pressure/bloggerapidemo/model/Blog.java
@@ -10,14 +10,44 @@ import com.google.gson.annotations.SerializedName;
 public class Blog implements Parcelable
 {
 
+    @SerializedName("kind")
+    @Expose
+    private String kind;
     @SerializedName("id")
     @Expose
     private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("published")
+    @Expose
+    private String published;
+    @SerializedName("updated")
+    @Expose
+    private String updated;
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("selfLink")
+    @Expose
+    private String selfLink;
+    @SerializedName("posts")
+    @Expose
+    private Posts posts;
+    @SerializedName("pages")
+    @Expose
+    private Pages pages;
+    @SerializedName("locale")
+    @Expose
+    private Locale locale;
     public final static Parcelable.Creator<Blog> CREATOR = new Creator<Blog>() {
 
 
         @SuppressWarnings({
-            "unchecked"
+                "unchecked"
         })
         public Blog createFromParcel(Parcel in) {
             return new Blog(in);
@@ -28,13 +58,31 @@ public class Blog implements Parcelable
         }
 
     }
-    ;
+            ;
 
     protected Blog(Parcel in) {
+        this.kind = ((String) in.readValue((String.class.getClassLoader())));
         this.id = ((String) in.readValue((String.class.getClassLoader())));
+        this.name = ((String) in.readValue((String.class.getClassLoader())));
+        this.description = ((String) in.readValue((String.class.getClassLoader())));
+        this.published = ((String) in.readValue((String.class.getClassLoader())));
+        this.updated = ((String) in.readValue((String.class.getClassLoader())));
+        this.url = ((String) in.readValue((String.class.getClassLoader())));
+        this.selfLink = ((String) in.readValue((String.class.getClassLoader())));
+        this.posts = ((Posts) in.readValue((Posts.class.getClassLoader())));
+        this.pages = ((Pages) in.readValue((Pages.class.getClassLoader())));
+        this.locale = ((Locale) in.readValue((Locale.class.getClassLoader())));
     }
 
     public Blog() {
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
     }
 
     public String getId() {
@@ -45,8 +93,90 @@ public class Blog implements Parcelable
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPublished() {
+        return published;
+    }
+
+    public void setPublished(String published) {
+        this.published = published;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getSelfLink() {
+        return selfLink;
+    }
+
+    public void setSelfLink(String selfLink) {
+        this.selfLink = selfLink;
+    }
+
+    public Posts getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Posts posts) {
+        this.posts = posts;
+    }
+
+    public Pages getPages() {
+        return pages;
+    }
+
+    public void setPages(Pages pages) {
+        this.pages = pages;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+
     public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(kind);
         dest.writeValue(id);
+        dest.writeValue(name);
+        dest.writeValue(description);
+        dest.writeValue(published);
+        dest.writeValue(updated);
+        dest.writeValue(url);
+        dest.writeValue(selfLink);
+        dest.writeValue(posts);
+        dest.writeValue(pages);
+        dest.writeValue(locale);
     }
 
     public int describeContents() {
