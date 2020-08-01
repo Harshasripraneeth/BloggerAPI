dc0fe4509420fcbb075faf9581b636f5d9cd45b9
Harshasripraneeth <46346478+Harshasripraneeth@users.noreply.github.com>
2020-08-01 16:43:57 +0530

    Date converter

---
+++ BloggerAPIDemo/app/src/main/java/com/pressure/bloggerapidemo/utilities/DateConversionUtil.java
@@ -0,0 +1,21 @@
+package com.pressure.bloggerapidemo.utilities;
+
+import android.util.Log;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+public class DateConversionUtil {
+
+    //converting date to String
+    SimpleDateFormat outputFormat = new SimpleDateFormat("dd-MM-yyyy");
+    //Parsing the given String to Date object
+    DateFormat innputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+    public  String dateConversion(String input) {
+        try {
+            return outputFormat.format(innputFormat.parse(input));
+        } catch (Exception e) {
+            Log.d("Format Exception", e.toString());
+        }
+        return input;
+    }
+}
