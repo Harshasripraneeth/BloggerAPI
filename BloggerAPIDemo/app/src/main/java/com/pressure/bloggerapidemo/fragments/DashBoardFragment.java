8e060481fa5c60f77ac1c04e73494347a6295ac4
Harshasripraneeth <46346478+Harshasripraneeth@users.noreply.github.com>
2020-08-04 19:34:03 +0530

    added navigation drawer

---
+++ BloggerAPIDemo/app/src/main/java/com/pressure/bloggerapidemo/fragments/DashBoardFragment.java
@@ -0,0 +1,20 @@
+package com.pressure.bloggerapidemo.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.pressure.bloggerapidemo.R;
+
+public class DashBoardFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dashboard,container,false);
+    }
+}
